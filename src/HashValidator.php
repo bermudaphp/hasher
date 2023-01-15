36f3b2cb07a2126685bf9a5eaf926ab45149cfec
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-01-15 14:57:36 +0200

    Update HashValidator.php

--- src/HashValidator.php
+++ src/HashValidator.php
@@ -4,5 +4,6 @@ namespace Bermuda\Hasher;
 
 interface HashValidator
 {
+    public function needRehash(string $hash): bool ;
     public function validateHash(string $input, string $hash): bool ;
 }
